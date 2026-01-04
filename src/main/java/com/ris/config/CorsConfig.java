e199d254cea4a173c25b55607cf0e47bfab07368
CelesteenaGeorge <celesteenajaya@gmail.com>
2026-01-04 01:21:33 +0000

    Fix: Enabled  CORS only for local development

--- src/main/java/com/ris/config/CorsConfig.java
+++ src/main/java/com/ris/config/CorsConfig.java
@@ -2,9 +2,11 @@ package com.ris.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("local")
 @Configuration
 public class CorsConfig {
     @Bean
